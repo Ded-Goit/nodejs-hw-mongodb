f6e1a416e91f90768268a783298537feb3f29927
Andriy Romanov <mgm.agro04@gmail.com>
2025-09-10 14:48:57 +0300

    edit auth routers

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -14,31 +14,36 @@ import {
   requestResetEmailSchema,
   resetPasswordSchema,
 } from '../validation/auth.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const authRouter = Router();
 
 authRouter.post(
   '/register',
   validateBody(registerUserSchema),
-  registerUserController,
+  ctrlWrapper(registerUserController),
 );
 
-authRouter.post('/login', validateBody(loginUserSchema), loginUserController);
+authRouter.post(
+  '/login',
+  validateBody(loginUserSchema),
+  ctrlWrapper(loginUserController),
+);
 
-authRouter.post('/refresh', refreshUserSessionController);
+authRouter.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
-authRouter.post('/logout', logoutUserController);
+authRouter.post('/logout', ctrlWrapper(logoutUserController));
 
 authRouter.post(
   '/send-reset-email',
   validateBody(requestResetEmailSchema),
-  requestResetEmailController,
+  ctrlWrapper(requestResetEmailController),
 );
 
 authRouter.post(
   '/reset-pwd',
   validateBody(resetPasswordSchema),
-  resetPasswordController,
+  ctrlWrapper(resetPasswordController),
 );
 
 export default authRouter;
